9e1ac8285804beba125c721b8afb4e3424d810d9
rickyota <22293266+rickyota@users.noreply.github.com>
2024-12-23 17:06:03 +0900

    add:

---
+++ mdx/app/ua_1.5.33.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# "upload agent" program for ukb rap (uk biobank research analysis platform)
+# https://documentation.dnanexus.com/downloads#Upload-Agent
+
+# The latest is v1.5.33 [ref](https://github.com/dnanexus/dx-toolkit/blob/master/src/ua/CHANGELOG)
+
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/large/ricky/app/
+APP=ua
+VER=1.5.33
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+mkdir -p $VER && cd $VER
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+wget \
+	https://dnanexus-sdk.s3.amazonaws.com/dnanexus-upload-agent-${VER}-linux.tar.gz -O - |
+	tar -xzf -
+
+mv ./dnanexus-upload-agent-1.5.33-linux/ua .
+rm -r ./dnanexus-upload-agent-1.5.33-linux/
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+-- Package settings
+prepend_path("PATH", apphome)
+__END__
