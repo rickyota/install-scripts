30da7d6e5ccef4ae48b9c91a96ded70ff5455545
rickyota <22293266+rickyota@users.noreply.github.com>
2025-04-23 10:00:17 +0900

    add:

---
+++ mdx/app/vg_1.52.0.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source /large/share/app/lmod/bash
+
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/large/ricky/app/
+APP=vg
+VER=1.52.0
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+mkdir $VER
+cd $VER
+wget https://github.com/vgteam/vg/releases/download/v${VER}/vg
+chmod +x vg
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+-- Package settings
+prepend_path("PATH", apphome)
+__END__
